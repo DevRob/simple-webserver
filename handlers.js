9affeefa76c57c5e8c61a6d14d32e739fafe8f00
Robert Molnar <mail.robertmolnar@gmail.com>
2016-12-13 17:04:53 +0000

    IP: tracker added

--- handlers.js
+++ handlers.js
@@ -1,6 +1,8 @@
+const axios = require("axios");
+const _ = require('lodash');
 exports.homepage = function(req, res) {
   res.setHeader("Content-Type", "text/html");
-  res.end("<h2>Hi There</h2>");
+  res.end("<h2>Hi There,</h2>");
 }
 
 exports.profile = function(req, res) {
@@ -16,4 +18,30 @@ exports.notFound = function(req, res) {
   res.statusCode = 404;
   res.setHeader("Content-Type", "text/html");
   res.end("<h2>404 Not found!</h2>");
+}
+
+exports.ipinfo = function(req, res) {
+  var ipAddress = req.connection.remoteAddress;
+  axios.get("http://freegeoip.net/json/" + ipAddress)
+      .then((api_result) => {
+
+    var ip = api_result.data["ip"],
+        code = api_result.data["country_code"],
+        country = api_result.data["country_name"],
+        city = api_result.data["city"];
+
+    console.log(ip);
+    res.setHeader("Content-Type", "text/html");
+    res.end("<div>" +
+      "<h2>Visiting from: " + country + "</h2>" +
+      "<ul>" +
+        "<li>IP: " + ip + "</li>" +
+        "<li>code: " + code + "</li>" +
+        "<li>city: " + city + "</li>" +
+      "<ul/>" +
+    "</div>");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 }
--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const handlers = require('./handlers');
 
 var server = http.createServer(function(req , res) {
   var url = req.url;
-
-  if (url === "/") {
-    handlers.homepage(req, res);
-  } else if (url === "/profile") {
-    handlers.profile(req, res);
-  } else {
-    handlers.notFound(req, res);
+  
+  switch(url) {
+    case "/profile":
+      handlers.profile(req, res);
+      break;
+    case "/":
+      handlers.homepage(req, res);
+      break;
+    case "/ip":
+      handlers.ipinfo(req, res);
+      break;
+    default:
+      handlers.notFound(req, res);
   }
 });
 
